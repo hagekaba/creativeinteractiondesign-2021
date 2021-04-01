22a533fab30c547673a512ba7369e6798b248080
zhanghanwen <2629890252@qq.com>
2021-04-01 11:47:01 +0800

    example5

---
+++ script.js
@@ -0,0 +1,53 @@
+document.write("Hello,word!");
+function sayHello(){
+ var response = prompt("What is your name?");
+ alert("Hello " + response + ", have a good day!");
+}
+function add(a,b){
+ return a + b;
+}
+console.log(add(20,30));
+console.log(add(100,30));
+function showTopic(){
+ var x = document.getElementById("demo");
+ x.style.fontSize = "25px";
+ x.style.color = "red";
+}
+var movies = ["Avengers","Captain Marvel",
+ "Black panther", "Spiderman"];
+ movies[0]="wonder woman"
+ movies.push("Avengers");
+ movies.pop();
+ console.log(movies);
+
+ var movie={
+   title:"Captain Marvel",
+   time:"2pm"
+ }
+ alert(movie.title);
+movie.status = "unavailable";
+console.log(movie);
+
+var movieDetails = [
+{title: "Avengers",
+ time:"12pm",
+ status: "available"},
+{title: "Captain Marvel",
+ time:"2pm",
+ status: "unavailable"},
+{title: "Black Panther",
+ time:"4pm",
+ status: "available"},
+{title: "Spiderman",
+ time:"6pm",
+ status: "available"}
+];
+
+for (var movie of movieDetails){
+ if (movie.status === "available"){
+ console.log("The movie " + movie.title + " plays at " + movie.time)
+ }
+else{
+ console.log("Sorry, the movie " + movie.title + " is sold out.")
+ }
+};
